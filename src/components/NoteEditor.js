47239627737aec7265074706132fc11f783ebeb1
ruben <rubenusshd@gmail.com>
2018-12-05 22:05:51 +0100

    configured editor inlinestyle function

--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -4,43 +4,48 @@ import { Editor, EditorState, RichUtils } from "draft-js";
 class NoteEditor extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      editorState: EditorState.createEmpty()
-    };
-
-    this.onChange = this.onChange.bind(this);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = editorState => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
     this._onClick = this._onClick.bind(this);
   }
-
-  onChange(editorState) {
-    this.setState({ editorState });
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return "handled";
+    }
+    return "not-handled";
   }
-
   _onClick(e) {
     this.onChange(
-      RichUtils.toggleInlineStyle(this.state.editorState, e.target.name)
+      RichUtils.toggleInlineStyle(this.state.editorState, e.target.value)
     );
   }
-
   render() {
-    const styles = ["BOLD", "ITALIC", "UNDERLINE", "CODE"];
+    const styles = ["BOLD", "UNDERLINE", "ITALIC", "CODE"];
     const buttons = styles.map(style => {
       return (
         <button
-          className="btn"
           key={style}
-          onClick={this._onClick}
           name={style}
+          className="btn"
+          onClick={this._onClick}
         >
-          {style}
+          {" "}
+          {style}{" "}
         </button>
       );
     });
     return (
       <div>
-        <div className="toolbar">{buttons}</div>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        {" "}
+        <div className="toolbar">{buttons}</div>{" "}
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />{" "}
       </div>
     );
   }
