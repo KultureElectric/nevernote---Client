edb0ca9ce59dfe1b00c7602aa52bfcf09f17de75
ruben <rubenusshd@gmail.com>
2018-12-09 14:17:28 +0100

    added logic that one can't acces dashboard when not authenticated

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import * as actions from "../actions";
@@ -18,15 +18,30 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Route component={Header} />
-          <Route exact path="/" component={Landing} />
-          <Route path="/dashboard" component={Dashboard} />
+          <Route
+            exact
+            path="/"
+            component={() =>
+              this.props.auth ? <Redirect to="/dashboard" /> : <Landing />
+            }
+          />
+          <Route
+            path="/dashboard"
+            component={() =>
+              !this.props.auth ? <Redirect to="/" /> : <Dashboard />
+            }
+          />
         </div>
       </BrowserRouter>
     );
   }
 }
 
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(App);
