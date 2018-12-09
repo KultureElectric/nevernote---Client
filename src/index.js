9cecb9575ada6db5b0ad212e7593a7ffb856fda9
ruben <rubenusshd@gmail.com>
2018-12-09 14:53:04 +0100

    quickly adde axios for use in browser console

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import App from "./components/App";
 import reducers from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
