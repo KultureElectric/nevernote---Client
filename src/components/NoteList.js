3e396a33bf48d1acd05eefb36dcaf7e643526d6a
ruben <rubenusshd@gmail.com>
2018-12-09 19:44:15 +0100

    connected store to component and logged object of notes

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class NoteList extends Component {
   render() {
+    console.log(this.props.editorState);
     return <div>A list of notes</div>;
   }
 }
 
-export default NoteList;
+const mapStateToProps = ({ editorState }) => {
+  return { editorState };
+};
+
+export default connect(mapStateToProps)(NoteList);
