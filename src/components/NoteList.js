f4a2b6e3e92020bf15d8220afbe517a7c5c973e6
ruben <rubenusshd@gmail.com>
2018-12-10 19:23:12 +0100

    managed to show a list of noted that are customized and clickable

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
+
+import * as actions from "../actions";
 
 class NoteList extends Component {
+  componentDidMount() {
+    this.props.fetchNotes();
+  }
+
+  handleNoteSelect(note) {
+    // perfect time to get a note shown
+    console.log(note);
+  }
+
+  renderNotes() {
+    const { notes } = this.props;
+    return _.map(notes, note => {
+      return (
+        <a
+          key={note.noteKey}
+          className="note-item card-content collection-item"
+          onClick={() => this.handleNoteSelect(note)}
+        >
+          <h1 className="card-title">Title</h1>
+          <div className="card-action">{note.body.blocks[0].text}</div>
+          <p>{new Date(note.lastUpdated).toLocaleDateString()}</p>
+        </a>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.editorState);
-    return <div>A list of notes</div>;
+    return <div className="card collection">{this.renderNotes()}</div>;
   }
 }
 
-const mapStateToProps = ({ editorState }) => {
-  return { editorState };
+const mapStateToProps = ({ notes }) => {
+  return { notes };
 };
 
-export default connect(mapStateToProps)(NoteList);
+export default connect(
+  mapStateToProps,
+  actions
+)(NoteList);
